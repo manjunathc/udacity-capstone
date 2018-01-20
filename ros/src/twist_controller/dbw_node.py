96233af205cc010d8a84de7340cfa797956eacf7
Feng Liu <feng3245@gmail.com>
2018-01-19 22:28:07 -0800

    Fixed syntax errors. Found out it eventually steered but was too late and steered very violently.

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -55,9 +55,9 @@ class DBWNode(object):
                                             ThrottleCmd, queue_size=1)
         self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
                                          BrakeCmd, queue_size=1)
-	self.dbw_enable_sub = rospy.Subscribe('/vehicle/dbw_enabled', Boolean,  dbw_enabled_callback)
-	self.current_velocity_sub = rospy.Subscribe('/current_velocity', TwistStamped, current_velocity_callback)
-	self.twist_cmd_sub = rospy.Subscribe('/twist_cmd', TwistStamped, twist_cmd_callback)
+	self.dbw_enable_sub = rospy.Subscriber('/vehicle/dbw_enabled', Bool,  self.dbw_enabled_callback)
+	self.current_velocity_sub = rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_callback)
+	self.twist_cmd_sub = rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_callback)
         # TODO: Create `TwistController` object
 
         self.controller = Controller(wheel_base,steer_ratio,0,max_lat_accel,max_steer_angle)
@@ -99,13 +99,13 @@ class DBWNode(object):
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
-    def dbw_enable_callback(data):
+    def dbw_enabled_callback(self, data):
 	self.dbw_enabled = data
 
-    def current_velocity_callback(data):
+    def current_velocity_callback(self, data):
         self.current_velocity = data.twist.linear.x
 
-    def twist_cmd_callback(data):
+    def twist_cmd_callback(self, data):
         self.linear_velocity = data.twist.linear.x
         self.angular_velocity = data.twist.angular.z
 
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -7,12 +7,14 @@ class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
 	# testing initialization is not giving error
-	self.Lowpass = LowPassFilter(0.3,1)
-	self.PID = PID(1,1,1)
+	
+	#self.Lowpass = LowPassFilter(0.3,1)
+	#self.PID = PID(1,1,1)
+	
 	self.YawCtrl = YawController(args[0], args[1], args[2], args[3], args[4])
         pass
 
     def control(self, *args, **kwargs):
-        steer = self.yawctrl.get_steering(args[0], args[1], args[2])
+        steer = self.YawCtrl.get_steering(args[0], args[1], args[2])
         # Return throttle, brake, steer
         return 0.2, 0., steer

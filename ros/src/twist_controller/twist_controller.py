cfd5c0f5c370035d46399a5737410a2aca27ea7f
Samuel H <emoogila@gmail.com>
2018-01-25 11:01:31 -0800

    Fix twist controller to use a single PID, instead of two.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,9 +1,9 @@
 import rospy
 from yaw_controller import YawController
 from pid import PID
+import time;
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-import time;
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
@@ -12,18 +12,22 @@ class Controller(object):
 	self.stopWatch = time.time()*1000
 	#self.Lowpass = LowPassFilter(0.3,1)
 	self.PIDThrottle = PID(0.8,0,0.05)
-	self.PIDBrake = PID(0.15,0,0.001)
 	self.YawCtrl = YawController(args[0], args[1], args[2], args[3], args[4])
-        pass
 
-    def control(self, *args, **kwargs):
-	accel = 0
-	brake = 0
-        steer = self.YawCtrl.get_steering(args[0], args[1], args[2])
-	if args[0] > args[2]:
-		accel = self.PIDThrottle.step(args[0]-args[2], time.time()*1000-self.stopWatch)
-	if args[0] < args[2]:
-		brake = self.PIDBrake.step(args[0]-args[2], time.time()*1000-self.stopWatch)
-	self.stopWatch = time.time()*1000
+    def control(self, linear_velocity, angular_velocity, current_velocity):
+        steer = self.YawCtrl.get_steering(linear_velocity, angular_velocity, current_velocity)
+
+        time_delta = time.time()*1000 - self.stopWatch
+	self.stopWatch += time_delta
+        accel = self.PIDThrottle.step(linear_velocity-current_velocity, time_delta)
+
+        if accel > 0:
+            throttle = accel
+            breaking = 0.
+        else:
+            breaking = -accel
+            throttle = 0.
+       
         # Return throttle, brake, steer
-        return accel%1, brake%5.0, steer
+        return throttle, breaking, steer
+
